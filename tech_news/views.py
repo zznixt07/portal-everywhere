ea4887f692d3208e3e15b64169a8b7b9b32d00a9
unknown <zznixt07@protonmail.com>
2021-05-06 00:57:28 +0545

    repeat this

--- tech_news/views.py
+++ tech_news/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from .logic import api as tech 
 from own_custom_app.decorators import has_correct_auth_header
 
-@has_correct_auth_header
+# @has_correct_auth_header
 def xda(request):
     resp = tech.xdadev_feed()
     return JsonResponse(resp, safe=False)
