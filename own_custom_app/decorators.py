965fbf0c2583f9628dd396b6be66431c0c0c3aca
unknown <zznixt07@protonmail.com>
2021-05-06 01:01:58 +0545

    repeat this

--- own_custom_app/decorators.py
+++ own_custom_app/decorators.py
@@ -6,7 +6,7 @@ def has_correct_auth_header(function):
     @wraps(function)
     def check_header(request, *args, **kwargs):
         rand_key = '96a768f388be5aaa8416c361570d053611357724f1b9'
-        if request.META.get('HTTP_AUTHORIZATION') == 'Basic: ' + rand_key:
+        if request.META.get('HTTP_AUTHORIZATION') == 'Basic ' + rand_key:
             return function(request, *args, **kwargs)
         else:
             return PermissionDenied
