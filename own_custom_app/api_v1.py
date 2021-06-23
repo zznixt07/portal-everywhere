78ab2a669db92b094ef5302dfdc54d482ef68002
zznixt07 <zznixt07@protonmail.com>
2021-06-23 11:11:25 +0545

    remove campus path

--- own_custom_app/api_v1.py
+++ own_custom_app/api_v1.py
@@ -4,6 +4,5 @@ from django.urls import path, include
 app_name = 'api_v1_app'
 urlpatterns = [
     path('', lambda: 1, name='index'),
-    path('campus/', include('campus.urls', namespace='campus')),
     path('tech/', include('tech_news.urls', namespace='tech_news')),
 ]
